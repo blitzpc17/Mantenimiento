276d6acd911e2afee97a386fdac807b407141141
blitzpc17 <remizeroaw117@gmail.com>
2025-06-04 16:58:51 -0600

    Eliminacion y restauracion de inventario y peticiones.

--- src/main/java/com/example/demo/controller/PeticionesController.java
+++ src/main/java/com/example/demo/controller/PeticionesController.java
@@ -27,6 +27,7 @@ import com.example.demo.dto.PeticionesDTO;
 import com.example.demo.entity.InventarioEntity;
 import com.example.demo.entity.PeticionesEntity;
 import com.example.demo.entity.UserEntity;
+import com.example.demo.excepciones.MiException;
 import com.example.demo.repository.CategoryRepository;
 import com.example.demo.repository.InventarioRepository;
 import com.example.demo.repository.PeticionesRepository;
@@ -51,182 +52,6 @@ public class PeticionesController {
 
 	@Autowired
 	private PeticionesRepository repopeti;
-
-	// @Autowired
-	// private PdfServicio pdfServicio;
-
-	/*
-	 * @GetMapping("/newPeticion")
-	 * public String index(Model model) {
-	 * List<PeticionesEntity> peticion = repopeti.findAll();
-	 * model.addAttribute("peticiones", peticion);
-	 * return "InsertPeticion";
-	 * }
-	 */
-
-	@GetMapping("/nombre/peticion/addpeticion")
-	public String handleGetPeticion(Model model) {
-		model.addAttribute("mensaje", "Por favor, envía el formulario usando POST.");
-		return "InsertPeticion";
-	}
-
-	@PostMapping(value = "/nombre/peticion/addpeticion")
-	public String peticion(@RequestParam("id") Integer idmain,
-			@RequestParam("nombre_del_solicitante") String nomsol,
-			@RequestParam("area_solicitada") String aresol,
-			@RequestParam("cantidad_solicitada") Integer cansol,
-			@RequestParam("fecha_de_peticion") String fepe,
-			@RequestParam("especificacion") String espe,
-			@RequestParam("fecha_de_elaboracion") String feel,
-			Model model) {
-
-		System.out.println("Area_solicitada: " + aresol);
-		System.out.println("Nombre_del_solicitante: " + nomsol);
-		System.out.println("Cantidad_solicitada: " + cansol);
-		System.out.println("Fecha_de_peticion: " + fepe);
-		System.out.println("Especificacion: " + espe);
-		System.out.println("Fecha_de_elaboracion: " + feel);
-
-		// Optional<InventarioEntity> inventariopeticionactualizada =
-		// InventarioRepository.findById(idmain);
-		Optional<InventarioEntity> inventariopeticionactualizada = repoIn.findById(idmain);
-
-		if (inventariopeticionactualizada.isPresent()) {
-			InventarioEntity inventario = inventariopeticionactualizada.get();
-
-			int cantidadDisponible = inventario.getTocan();
-
-			if (cantidadDisponible >= cansol) {
-
-				int resta = cantidadDisponible - cansol;
-
-				inventario.setTocan(resta);
-				repoIn.save(inventario);
-
-				PeticionesEntity nuevaPeticion = new PeticionesEntity();
-				nuevaPeticion.setAresol(aresol);
-				nuevaPeticion.setNomsol(nomsol);
-				nuevaPeticion.setCansol(cansol);
-				nuevaPeticion.setFepe(fepe);
-				nuevaPeticion.setEspe(espe);
-				nuevaPeticion.setFeel(feel);
-				nuevaPeticion.setCantidis(resta);
-
-				nuevaPeticion.setTotalcan(inventario);
-
-				repopeti.save(nuevaPeticion);
-
-				model.addAttribute("mensaje", "Petición agregada correctamente.");
-				// return "redirect:/nombre/buscar";
-				return "redirect:/listadeeptcionesactualizables";
-			} else {
-				model.addAttribute("mensaje", "No hay suficiente inventario disponible.");
-				return "InsertPeticion";
-			}
-		} else {
-			// Si el inventario no existe, maneja la situación
-			model.addAttribute("mensaje", "Inventario no encontrado.");
-			return "InsertPeticion";
-		}
-	}
-
-	/*
-	 * if (!aresol.isEmpty() && !nomsol.isEmpty()) {
-	 * PeticionesEntity PeticionesEntity = new PeticionesEntity();
-	 * PeticionesEntity.setAresol(aresol);
-	 * PeticionesEntity.setNomsol(nomsol);
-	 * PeticionesEntity.setCansol(cansol);
-	 * PeticionesEntity.setFepe(fepe);
-	 * PeticionesEntity.setEspe(espe);
-	 * PeticionesEntity.setFeel(feel);
-	 * 
-	 * 
-	 * repopeti.saveAndFlush(PeticionesEntity);
-	 * 
-	 * model.addAttribute("mensaje", "Registro de: " + cansol + " agregada!");
-	 * return index(model);
-	 * 
-	 * }
-	 * model.addAttribute("mensaje", "Valores erroneos");
-	 * return "InsertPeticion";
-	 * }
-	 */
-
-	/*
-	 * @GetMapping("/peticionesyactualizaciones")
-	 * public String listarinventario(Model model) {
-	 * List<InventarioEntity> materialoficial = repoIn.findAll();
-	 * // List<Category> categoriaoficial = repocate.findAll();
-	 * model.addAttribute("inventarios", materialoficial);
-	 * // model.addAttribute("categorias",categoriaoficial);
-	 * return "InventaryListPeticActual";
-	 * 
-	 * }
-	 */
-
-	@GetMapping("/listadeeptcionesactualizables")
-	public String listarptcionesactualizables(Model model) {
-		List<PeticionesEntity> peticionactualizada = repopeti.findAll();
-		// List<Category> categoriaoficial = repocate.findAll();
-		model.addAttribute("peticiones", peticionactualizada);
-		// model.addAttribute("categorias",categoriaoficial);
-		return "PeticionesList";
-	}
-
-	@PostMapping("/peticion/buscar")
-	public String search(@RequestParam("buscar") String nomsol, Model model) {
-		List<PeticionesEntity> encotrarpeticion = repopeti.findByNomsolContainingIgnoreCase(nomsol);
-
-		if (encotrarpeticion.isEmpty()) {
-
-			return listarptcionesactualizables(model);
-		}
-		model.addAttribute("inventarios", encotrarpeticion);
-		return "PeticionesList";
-	}
-
-	@PostMapping("/peticion/eliminar/{id}")
-	public String delete(@PathVariable(name = "id") Integer idpeti, Model model) {
-		Optional<PeticionesEntity> peticionoficial = repopeti.findById(idpeti);
-
-		if (peticionoficial.isPresent()) {
-			PeticionesEntity eliminarpeticion = peticionoficial.get();
-			repopeti.delete(eliminarpeticion);
-
-		}
-		return listarptcionesactualizables(model);
-	}
-	/*
-	 * @GetMapping("/peticion/reporte/{id}")
-	 * public ResponseEntity<byte[]> generarReportePdf(@PathVariable("id") Integer
-	 * id) {
-	 * Optional<PeticionesEntity> optionalPeticion = repopeti.findById(id);
-	 * 
-	 * if (optionalPeticion.isPresent()) {
-	 * PeticionesEntity peticion = optionalPeticion.get();
-	 * 
-	 * try {
-	 * byte[] pdfBytes = pdfServicio.generarReportePDF(peticion);
-	 * 
-	 * HttpHeaders headers = new HttpHeaders();
-	 * headers.setContentType(MediaType.APPLICATION_PDF);
-	 * headers.setContentDispositionFormData("filename", "reporte_peticion_" + id +
-	 * ".pdf");
-	 * 
-	 * return ResponseEntity.ok()
-	 * .headers(headers)
-	 * .body(pdfBytes);
-	 * 
-	 * } catch (IOException e) {
-	 * e.printStackTrace();
-	 * return ResponseEntity.internalServerError().build();
-	 * }
-	 * 
-	 * } else {
-	 * return ResponseEntity.notFound().build();
-	 * }
-	 * }
-	 */
 
 	@GetMapping("/index")
 	public String indexPrecargado(@RequestParam("materialId") Integer materialId, Model model) {
@@ -312,13 +137,30 @@ public class PeticionesController {
 	}
 
 	@PostMapping("/delete")
-	public ResponseEntity<?> EliminarRegistro(@RequestBody InventarioDTO obj) {
+	public ResponseEntity<?> EliminarRegistro(@RequestBody PeticionesDTO obj)throws MiException {
 
-		System.out.println(obj.getIdMateriales());
+		System.out.println(obj.getPeticionesId());
 
 		try {
 
-			inventarioServicio.eliminar(obj.getIdMateriales());
+			//elimina y restaura la piezas tomadas
+
+			PeticionesEntity objP = repopeti.findById(obj.getPeticionesId())
+			.orElseThrow(() -> new MiException("Registro no encontrado con ID: " + obj.getPeticionesId()));
+
+			InventarioEntity objMaterial = repoIn.findById(objP.getTotalcan().getId())
+			.orElseThrow(() -> new MiException("Registro no encontrado con ID: " + obj.getPeticionesId()));
+
+			int cantidad = objMaterial.getTocan();
+
+			cantidad = cantidad + objP.getCansol();
+
+			objMaterial.setTocan(cantidad);
+
+			repoIn.save(objMaterial);
+
+			repopeti.delete(objP);
+			
 
 			return ResponseEntity.ok().body("Registro eliminado correctamente");
 
